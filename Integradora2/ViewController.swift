3d36baf9f60785e0067676a713520f5c0ab2eee4
DioBrandoNox <23170129@uttcampus.edu.mx>
2025-11-16 14:00:50 -0600

    Initial Commit

---
+++ Integradora2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Integradora2
+//
+//  Created by Brando Ariel De Los Santos Guzman on 16/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
